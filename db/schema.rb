e6b447cefaf481caaf147cb964e282275d7704e5
RobinTersou <tersou.robin@gmail.com>
2018-02-02 18:36:56 +0100

    Correction db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110092422) do
+ActiveRecord::Schema.define(version: 20180110094706) do
 
   create_table "classes", force: :cascade do |t|
     t.string "name"
     t.integer "nbstudents"
     t.integer "number"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.date "birthDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
